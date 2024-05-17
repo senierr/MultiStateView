c821a076983c3f31fd9df3ba2f2c4c664d3a73d5
chunjiezhou <chunjiezhou@pateo.com.cn>
2024-05-17 10:37:17 +0800

    初始化项目

---
+++ app/src/main/java/com/senierr/test/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.senierr.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 首页
+ *
+ * @author senierr_zhou
+ * @date 2023/07/28
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
